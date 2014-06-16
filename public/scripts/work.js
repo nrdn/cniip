8454fb7048af7c539d178b87f5385f02708d6c64
ponyspy <desade4me@gmail.com>
2014-06-16 19:09:30 +0400

    drag

--- public/scripts/work.js
+++ public/scripts/work.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+	$(document).on('dragstart', function(e) {
+		if (e.target.nodeName.toUpperCase() == 'IMG') {
+			return false;
+		}
+	});
+
 	$('.content_block').mapNavigate({
 	    speed: 500
 	});

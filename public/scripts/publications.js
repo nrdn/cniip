c7709db0a1355e668a848c984a5b3fa1a092c921
ponyspy <desade4me@gmail.com>
2014-06-23 19:36:52 +0400

    pub

--- public/scripts/publications.js
+++ public/scripts/publications.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$('.publications_items').mousemove(function(event) {
+	$('.authors_items').mousemove(function(event) {
 		var scroller_offset = event.pageX - 25;
 		var scroller_width = $(document).width() - 60;
 
 		$(this).scrollLeft(event.pageX * 1.7);
 		if (scroller_offset >= 5 && scroller_offset <= scroller_width)
-			$('.pub_scroller').css('margin-left', event.pageX - 32)
+			$('.authors_scroller').css('margin-left', event.pageX - 32)
 	});
 });
